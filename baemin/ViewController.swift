c712f63b762afd785d8f3e11c1616a4d096d424d
minseongkim <kyrok67@naver.com>
2021-06-22 06:57:48 +0900

    메뉴 View 구성완료.

--- baemin/ViewController.swift
+++ baemin/ViewController.swift
@@ -8,7 +8,8 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    var adImages: [UIImage] = [#imageLiteral(resourceName: "슬라이드1"), #imageLiteral(resourceName: "슬라이드2"), #imageLiteral(resourceName: "슬라이드3"), #imageLiteral(resourceName: "슬라이드4"), #imageLiteral(resourceName: "슬라이드5"), #imageLiteral(resourceName: "슬라이드7"), #imageLiteral(resourceName: "슬라이드8"), #imageLiteral(resourceName: "슬라이드9"), #imageLiteral(resourceName: "슬라이드10"), #imageLiteral(resourceName: "슬라이드11"), #imageLiteral(resourceName: "슬라이드12"), #imageLiteral(resourceName: "슬라이드13"), #imageLiteral(resourceName: "슬라이드14"), #imageLiteral(resourceName: "슬라이드15"), #imageLiteral(resourceName: "슬라이드16"), #imageLiteral(resourceName: "슬라이드17")]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
