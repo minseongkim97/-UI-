0a4df2e23df80991c35baa71d5763976ae51ebc1
minseongkim <kyrok67@naver.com>
2021-06-24 12:52:28 +0900

    UITabBarController 사용.

--- baemin/CustomTabBarController.swift
+++ baemin/CustomTabBarController.swift
@@ -8,5 +8,17 @@
 import UIKit
 
 class CustomTabBarController: UITabBarController {
-    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // 모서리 둥글게
+        tabBar.layer.cornerRadius = 20
+        tabBar.layer.maskedCorners = [.layerMinXMinYCorner, .layerMaxXMinYCorner]
+        
+        // 그림자 넣기
+        tabBar.layer.shadowOffset = CGSize(width: 1, height: 2)
+        tabBar.layer.shadowRadius = 2
+        tabBar.layer.shadowColor = UIColor.black.cgColor
+        tabBar.layer.shadowOpacity = 0.07
+    }
 }
--- baemin/ViewController.swift
+++ baemin/ViewController.swift
@@ -8,11 +8,8 @@
 import UIKit
 
 class ViewController: UIViewController {
-    var adImages: [UIImage] = [#imageLiteral(resourceName: "슬라이드1"), #imageLiteral(resourceName: "슬라이드2"), #imageLiteral(resourceName: "슬라이드3"), #imageLiteral(resourceName: "슬라이드4"), #imageLiteral(resourceName: "슬라이드5"), #imageLiteral(resourceName: "슬라이드7"), #imageLiteral(resourceName: "슬라이드8"), #imageLiteral(resourceName: "슬라이드9"), #imageLiteral(resourceName: "슬라이드10"), #imageLiteral(resourceName: "슬라이드11"), #imageLiteral(resourceName: "슬라이드12"), #imageLiteral(resourceName: "슬라이드13"), #imageLiteral(resourceName: "슬라이드14"), #imageLiteral(resourceName: "슬라이드15"), #imageLiteral(resourceName: "슬라이드16"), #imageLiteral(resourceName: "슬라이드17")]
-    var saleImages: [UIImage] = []
+    let adImages: [UIImage] = [#imageLiteral(resourceName: "슬라이드1"), #imageLiteral(resourceName: "슬라이드2"), #imageLiteral(resourceName: "슬라이드3"), #imageLiteral(resourceName: "슬라이드4"), #imageLiteral(resourceName: "슬라이드5"), #imageLiteral(resourceName: "슬라이드7"), #imageLiteral(resourceName: "슬라이드8"), #imageLiteral(resourceName: "슬라이드9"), #imageLiteral(resourceName: "슬라이드10"), #imageLiteral(resourceName: "슬라이드11"), #imageLiteral(resourceName: "슬라이드12"), #imageLiteral(resourceName: "슬라이드13"), #imageLiteral(resourceName: "슬라이드14"), #imageLiteral(resourceName: "슬라이드15"), #imageLiteral(resourceName: "슬라이드16"), #imageLiteral(resourceName: "슬라이드17")]
     
-//    var saleImagesViews = [UIImageView]()
-//    var adImagesViews = [UIImageView]()
     
     @IBOutlet private weak var roundView: UIView! {
         didSet {
@@ -27,7 +24,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         addAdScrollView()
-//        addSaleScrollView()
+        
 
     }
     
@@ -38,26 +35,8 @@ class ViewController: UIViewController {
             let xPos = self.view.frame.width * CGFloat(i)
             imageView.frame = CGRect(x: xPos, y: 0, width: adScrollView.bounds.width, height: adScrollView.bounds.height)
             imageView.image = adImages[i]
-            imageView.contentMode = .scaleToFill
             adScrollView.addSubview(imageView)
-            adScrollView.contentSize.width = imageView.frame.width * CGFloat(i + 1)
         }
     }
-    
-//    private func addSaleScrollView() {
-//
-//        for i in 0..<saleImages.count {
-//            let imageView = UIImageView()
-//            let xPos = self.view.frame.width * CGFloat(i)
-//            imageView.frame = CGRect(x: xPos, y: 0, width: saleScrollView.bounds.width, height: saleScrollView.bounds.height)
-//            imageView.image = saleImages[i]
-//            imageView.contentMode = .scaleToFill
-//            saleScrollView.addSubview(imageView)
-//            saleScrollView.contentSize.width = imageView.frame.width * CGFloat(i + 1)
-//        }
-//    }
-    
-
-
 }
 
